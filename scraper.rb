b764ac237d909ca4b5f70366d8886d92e0c80a3c
Tony Bowden <tony@tmtm.com>
2015-09-28 16:37:57 +0100

    change start URL for scraper
    and bring it up to date with our current approach

--- scraper.rb
+++ scraper.rb
@@ -7,45 +7,45 @@ require 'date'
 require 'open-uri'
 require 'date'
 
-# require 'colorize'
-# require 'pry'
-# require 'csv'
-# require 'open-uri/cached'
-# OpenURI::Cache.cache_path = '.cache'
+require 'colorize'
+require 'pry'
+require 'csv'
+require 'open-uri/cached'
+OpenURI::Cache.cache_path = '.cache'
 
 def noko(url)
   Nokogiri::HTML(open(url).read) 
 end
 
-def datefrom(date)
-  Date.parse(date)
+def scrape_page(url)
+  page = noko(url)
+  page.xpath('//table[.//tr[contains(.,"Groupes Parlemenataires")]]//tr').drop(1).each do |party|
+    tds = party.css('td')
+    party_name = tds.first.text.strip
+    member_count = tds[2].text.gsub(/[[:space:]]/, ' ').strip.to_i
+    party_url = URI.join(url, tds.last.css('a/@href').text).to_s
+    scrape_party(party_url, party_name, member_count)
+  end
 end
 
-@BASE = 'http://www.assemblee.ne'
-url = @BASE + '/index.php/organes/les-groupes-parlementaires'
-
-page = noko(url)
-page.xpath('//table[.//tr[contains(.,"Membres")]]//tr').drop(1).each do |party|
-  tds = party.css('td')
-  party_name = tds.first.text.strip
-  member_count = tds[2].text.gsub(/[[:space:]]/, ' ').strip.to_i
-  party_url = @BASE + tds.last.css('a/@href').text
-
+def scrape_party(party_url, party_name, member_count)
+  warn party_url
   member_page = noko(party_url)
   mps = member_page.css('.article-content li')
   mps = member_page.css('.article-content p') if mps.count.zero?
-  raise "Should have #{member_count} MPs; have #{mps.count}" unless mps.count == member_count
+  warn "#{party_url} should have #{member_count} MPs; have #{mps.count}" unless mps.count == member_count
   mps.each do |mp|
     data = { 
       name: mp.text.gsub(/^\d+\.\s*/,'').upcase,
       party: party_name,
-      party_id: party_url.split('/').last[/^(\d+)/, 1], 
+      party_id: CGI.parse(URI.parse(party_url).query)['id'].first,
       gender: 'male',
       term: '7.1',
       source: party_url,
     }
     data[:gender] = 'female' if data[:name].gsub!(' (MME)', '')
-    puts data
     ScraperWiki.save_sqlite([:name, :term], data)
   end
 end
+
+scrape_page 'http://www.assemblee.ne/index.php?option=com_content&view=article&id=165&Itemid=154'
